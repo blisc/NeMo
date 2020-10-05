569d82fff02601fd08025468ee98759b93f0590c
Jason <jasoli@nvidia.com>
2020-10-05 16:02:30 -0700

    bug
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/tts/modules/tacotron2.py
+++ nemo/collections/tts/modules/tacotron2.py
@@ -244,7 +244,7 @@ class Decoder(NeuralModule):
 
         self.memory = memory
         with torch.cuda.amp.autocast(enabled=False):
-            self.processed_memory = self.attention_layer.memory_layer(memory)
+            self.processed_memory = self.attention_layer.memory_layer(memory.float())
         self.mask = mask
 
     def parse_decoder_inputs(self, decoder_inputs):
