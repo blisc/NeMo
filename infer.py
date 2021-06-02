5f3bb8a5b0196805b968f64d364f9e3eda655a87
Jason <jasoli@nvidia.com>
2021-06-02 07:37:34 -0700

    add argpars
    Signed-off-by: Jason <jasoli@nvidia.com>

--- infer.py
+++ infer.py
@@ -1,4 +1,5 @@
 import time
+import argparse
 
 import numpy as np
 import torch
@@ -7,6 +8,14 @@ from scipy.io.wavfile import write
 
 from nemo.collections.asr.data.audio_to_text import AudioToCharDataset, _AudioTextDataset
 from nemo.collections.tts.models import HifiGanModel, Tacotron2Model
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-bs', '--batchsize', type=int, default=1)
+parser.add_argument('-c', '--checkpoint', type=str)
+parser.add_argument('-m', '--manifest', type=str)
+
+args = parser.parse_args()
 
 
 class MeasureTime(list):
@@ -25,14 +34,14 @@ class MeasureTime(list):
 
 torch.backends.cudnn.benchmark = True
 
-t2 = Tacotron2Model.restore_from("/home/jasoli/nemo/NeMo/examples/tts/Tacotron2.nemo")
+t2 = Tacotron2Model.restore_from(args.checkpoint)
 t2 = t2.cuda().eval()
 t2.decoder.gate_threshold = 0.25
 labels = t2.cfg.labels
 
 
 dataset = _AudioTextDataset(
-    "/data/speech/HiFiTTS/8051_manifest_clean_test.json",
+    args.manifest,
     parser=t2.parser,
     sample_rate=44100,
     bos_id=len(labels),
@@ -40,7 +49,7 @@ dataset = _AudioTextDataset(
     pad_id=len(labels) + 2,
 )
 dataloader = torch.utils.data.DataLoader(
-    dataset=dataset, batch_size=32, collate_fn=dataset.collate_fn, num_workers=0, shuffle=False
+    dataset=dataset, batch_size=args.batchsize, collate_fn=dataset.collate_fn, num_workers=0, shuffle=False
 )
 measures = MeasureTime()
 device = torch.device("cuda")
@@ -52,7 +61,7 @@ for b in dataloader:
     batches.append(b)
 
 print("warming up model")
-for _ in tqdm.tqdm(range(10)):
+for _ in tqdm.tqdm(range(1)):
     for i, batch in tqdm.tqdm(enumerate(batches)):
         _, _, text, text_length = batch
         text = text.to(device)
@@ -61,7 +70,7 @@ for _ in tqdm.tqdm(range(10)):
 
 
 print("testing model")
-for _ in tqdm.tqdm(range(100)):
+for _ in tqdm.tqdm(range(2)):
     for batch in tqdm.tqdm(batches):
         _, _, text, text_length = batch
         text = text.to(device)
