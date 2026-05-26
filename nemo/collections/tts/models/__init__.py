44e9e4734837eccee93ad0cbad0e0c861162f287
Jason <jasoli@nvidia.com>
2026-05-26 13:48:42 -0700

    remove big import statements
    Signed-off-by: Jason <jasoli@nvidia.com>

--- nemo/collections/tts/models/__init__.py
+++ nemo/collections/tts/models/__init__.py
@@ -11,3 +11,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from nemo.collections.tts.models.aligner import AlignerModel
+from nemo.collections.tts.models.audio_codec import AudioCodecModel
+from nemo.collections.tts.models.easy_magpietts import EasyMagpieTTSModel
+from nemo.collections.tts.models.easy_magpietts_inference import EasyMagpieTTSInferenceModel
+from nemo.collections.tts.models.easy_magpietts_preference_optimization import EasyMagpieTTSModelOnlinePO
+from nemo.collections.tts.models.fastpitch import FastPitchModel
+from nemo.collections.tts.models.fastpitch_ssl import FastPitchModel_SSL
+from nemo.collections.tts.models.hifigan import HifiGanModel
+from nemo.collections.tts.models.magpietts import InferBatchOutput, MagpieTTSModel
+from nemo.collections.tts.models.magpietts_cfg_distillation import OnlineCFGDistillation
+from nemo.collections.tts.models.magpietts_preference_optimization import (
+    MagpieTTSModelOfflinePO,
+    MagpieTTSModelOfflinePODataGen,
+    MagpieTTSModelOnlinePO,
+)
+from nemo.collections.tts.models.ssl_tts import SSLDisentangler
+
+__all__ = [
+    "AlignerModel",
+    "AudioCodecModel",
+    "FastPitchModel",
+    "FastPitchModel_SSL",
+    "SSLDisentangler",
+    "HifiGanModel",
+    "InferBatchOutput",
+    "MagpieTTSModel",
+    "OnlineCFGDistillation",
+    "EasyMagpieTTSModel",
+    "EasyMagpieTTSInferenceModel",
+    "EasyMagpieTTSModelOnlinePO",
+    "MagpieTTSModelOfflinePODataGen",
+    "MagpieTTSModelOfflinePO",
+    "MagpieTTSModelOnlinePO",
+]
